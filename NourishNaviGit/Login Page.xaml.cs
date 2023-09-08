718e63062ed9578bc56f5f940feb888000cb1f3a
cbarina07 <S3965962@student.rmit.edu.au>
2023-09-08 17:24:33 +1000

    created login page text entries

--- NourishNaviGit/Login Page.xaml.cs
+++ NourishNaviGit/Login Page.xaml.cs
@@ -6,6 +6,18 @@ public partial class Login_Page : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        string oldText = e.OldTextValue;
+        string newText = e.NewTextValue;
+        string myText = entry.Text;
+    }
+
+    void OnEntryCompleted(object sender, EventArgs e)
+    {
+        string text = ((Entry)sender).Text;
+    }
 }
 
    
