260b60c644305688ec65b9d766e21e625865cdf6
s3839262 <s3839262@student.rmit.edu.au>
2023-09-12 15:30:54 +1000

    added placeholders for required functions for buttons in login and signup page, so the emulator should be able to build our program now. I have written comments explaining how it works, and where the placeholder text begins and ends, so you can delete/edit as you like!

--- NourishNaviGit/Signup Page.xaml.cs
+++ NourishNaviGit/Signup Page.xaml.cs
@@ -2,8 +2,22 @@ namespace NourishNaviGit;
 
 public partial class Signup_Page : ContentPage
 {
-	public Signup_Page()
+    //Placeholder: see Login Page for notes on how this works
+    private void OnCreateBtnClicked(object sender, EventArgs e)
+    {
+        CreateAccount.Text = yourFunction();
+    }
+    //end of placeholder
+    public Signup_Page()
 	{
 		InitializeComponent();
-	}
+       
+    }
+
+    //placeholder
+    public string yourFunction()
+    {
+        return "you clicked the button yay";
+    }
+    //end of placeholder
 }
