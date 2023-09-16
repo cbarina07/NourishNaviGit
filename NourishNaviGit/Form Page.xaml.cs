f0549c63237fe5531123b6500cd73e189a0cdd05
cbarina07 <S3965962@student.rmit.edu.au>
2023-09-16 21:58:41 +1000

    adding radio buttons and checkboxes along with starting off some styling

--- NourishNaviGit/Form Page.xaml.cs
+++ NourishNaviGit/Form Page.xaml.cs
@@ -11,7 +11,7 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-                CounterBtn.Text = writePrompt();
+                PromptBtn.Text = writePrompt();
 
         }
 
