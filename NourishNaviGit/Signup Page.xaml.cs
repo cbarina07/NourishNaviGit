01af987935ef8998c8861c66c5f3093b90e33920
cbarina07 <S3965962@student.rmit.edu.au>
2023-09-21 15:17:47 +1000

    deleted the header images

--- NourishNaviGit/Signup Page.xaml.cs
+++ NourishNaviGit/Signup Page.xaml.cs
@@ -1,21 +1,22 @@
-namespace NourishNaviGit;
-
-    public partial class Signup_Page : ContentPage
+namespace NourishNaviGit
 {
-    public Signup_Page()
+    public partial class Signup_Page : ContentPage
     {
-        InitializeComponent();
-    }
+        public Signup_Page()
+        {
+            InitializeComponent();
+        }
 
-    private async void OnCreateBtnClicked(object sender, EventArgs e)
-    {
-        CreateAccount.Text = await yourFunction();
-    }
+        private async void OnCreateBtnClicked(object sender, EventArgs e)
+        {
+            // Display "Account Created" on the button
+            CreateAccount.Text = "Account Created";
 
-    public async Task<string> yourFunction()
-    {
-        await Task.Delay(5000);
-        await Navigation.PushAsync(new MainPage());
-        return "Account Created";
+            // Delay for 5 seconds
+            await Task.Delay(2000);
+
+            // Navigate to the home page
+            await Navigation.PushAsync(new MainPage());
+        }
     }
 }
