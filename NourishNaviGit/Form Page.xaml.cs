d1981633ca4a15eee7144e46d4b04b8876d206ac
s3839262 <s3839262@student.rmit.edu.au>
2023-09-22 14:38:05 +1000

    fixing entrymeals again

--- NourishNaviGit/Form Page.xaml.cs
+++ NourishNaviGit/Form Page.xaml.cs
@@ -1,302 +1,135 @@
-﻿using NourishNaviGit;
-
-namespace NourishNaviGit;
-
-public partial class FormPage : ContentPage
-{
-    //int count = 0;
-
-    private Dictionary<string, List<RadioButton>> radioButtonGroups = new Dictionary<string, List<RadioButton>>();
-
-    public FormPage()
+﻿namespace NourishNaviGit;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+    public partial class FormPage : ContentPage
     {
-        InitializeComponent();
+        //int count = 0;
 
-        // Adding radio buttons to dictionary so that clicking a radio button in one section doesnt unclick radio buttons in other sections 
-        AddRadioButtonToGroup("Gender", MaleRadioButton);
-        AddRadioButtonToGroup("Gender", FemaleRadioButton);
-        AddRadioButtonToGroup("Gender", NonBinaryRadioButton);
-        AddRadioButtonToGroup("Gender", OtherRadioButton);
+        public FormPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnLoginBtnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login_Page());
+        }
+
+        private async void OnSignupBtnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Signup_Page());
+        }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+        {
+                PromptBtn.Text = writePrompt();
+
+        }
+
+ 
+
+        //Initialising variables - defaults to null so program can detect which inputs exist and should be included in the final prompt.
         
-        AddRadioButtonToGroup("Activity", NoActivityRadioButton);
-        AddRadioButtonToGroup("Activity", LittleActivityRadioButton);
-        AddRadioButtonToGroup("Activity", QuiteActiveRadioButton);
-        AddRadioButtonToGroup("Activity", VeryActiveRadioButton);
+        string userAge = "0";
+        string userGender = "demo"; //eg. Female
+        string userActivity = "demo"; //eg. Sedentary
+        string userMeals = "demo"; //eg. breakfast, dinner
+        string userDiet = "demo"; //eg. halal
+        string userAllergy = "demo"; //eg. nuts
 
-        AddRadioButtonToGroup("Diet", NoRestrictionsRadioButton);
-        AddRadioButtonToGroup("Diet", VegetarianRadioButton);
-        AddRadioButtonToGroup("Diet", PescetarianRadioButton);
-        AddRadioButtonToGroup("Diet", GlutenFreeRadioButton);
-        AddRadioButtonToGroup("Diet", HalalRadioButton);
-        AddRadioButtonToGroup("Diet", OtherDietRadioButton);
+        // These two address cravings for the week - an additional feature that makes this entry non-permanent in menu completion section.
+        string userDislikes = "demo"; //eg. broccoli
+        string userLikes = "demo"; //eg. pumpkin
 
+        //prompt section formats
+        static string sectionAge = "I am a ";
+        static string sectionActivity = ", my lifestyle is ";
+        static string sectionMeals = ", I want to eat ";
+        static string sectionDiet = ", my diet is ";
+        static string sectionAllergy = ", I can't have ";
+        static string sectionDislikes = ", I'd rather not have ";
+        static string sectionLikes = ", I'm feeling like ";
 
-        //UNCERTAIN IF I NEED THIS SO JUST UNCOMMENTING FOR NOW
-        //AddRadioButtonToGroup("Dislikes", NoDislikes);
-        //AddRadioButtonToGroup("Dislikes", YesDislikes);
+        //prompt sections
+        string promptAge;
+        string promptActivity;
+        string promptMeals;
+        string promptDiet;
+        string promptAllergy;
+        string promptDislikes;
+        string promptLikes;
 
-        //AddRadioButtonToGroup("Likes", Nolikes);
-        //AddRadioButtonToGroup("Likes", Yeslikes);
-
-
-
-        // adding check changed to each radio button
-        foreach (var group in radioButtonGroups.Values)
-        {
-            foreach (var radioButton in group)
+        //prompt types
+        static string promptOpen = "Write me a meal plan for the next week ";
+        static string promptSimple = "and give me only the headings of each meal, ";
+        string promptFull;
+        protected string writePrompt() {
+            // This design assumes the default value will be "None".
+            if (entrySimple.IsChecked != true)
             {
-                radioButton.CheckedChanged += RadioButton_CheckedChanged;
+                promptSimple = "";
             }
-        }
-    }
-
-    // Helper method to add radio buttons to a group in the dictionary
-    private void AddRadioButtonToGroup(string groupName, RadioButton radioButton)
-    {
-        if (!radioButtonGroups.ContainsKey(groupName))
-        {
-            radioButtonGroups[groupName] = new List<RadioButton>();
-        }
-
-        radioButtonGroups[groupName].Add(radioButton);
-    }
-
-    // Event handler for radio button checked changes
-    private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
-    {
-        if (sender is RadioButton radioButton && radioButton.IsChecked)
-        {
-            // Get the group name of the selected radio button
-            string groupName = radioButton.GroupName;
-
-            // Deselect other radio buttons in the same group
-            foreach (var otherRadioButton in radioButtonGroups[groupName].Where(rb => rb != radioButton))
+            userAge = entryAge.Text;
+            promptAge = sectionAge + userAge;
+            userGender = entryGender.Text;
+            userActivity = entryActivity.Text;
+ 
+            if (userActivity == "None")
             {
-                otherRadioButton.IsChecked = false;
-            }
-            // Check if the selected radio button is not the "Other (Please specify)" option
-            if (radioButton != OtherDietRadioButton)
-            {
-                // If anything is checked other than "Other," hide the entry field
-                entryDiet.IsVisible = false;
+                promptActivity = "";
             }
             else
             {
-                // If "Other" is checked, show the entry field
-                // means that people wont be able to select vegeterian and then also type something in "other"
-                entryDiet.IsVisible = true;
+                promptActivity = sectionActivity + userActivity;
             }
-
+            userMeals = entryMeals.Text;
+            if (userMeals == "None")
+            {
+                promptMeals = "";
+            }
+            else
+            {
+                promptMeals = sectionMeals + userMeals;
+            }
+            userDiet = entryDiet.Text;
+            if (userDiet == "None") {
+                promptDiet = "";
+            }
+            else
+            {
+                promptDiet = sectionDiet + userDiet;
+            }
+            userAllergy = entryAllergy.Text;
+            if (userAllergy == "None")
+            {
+                promptAllergy = "";
+            }
+            else
+            {
+                promptAllergy = sectionAllergy + userAllergy;
+            }
+            userDislikes = entryDislikes.Text;
+            if (userDislikes == "None")
+            {
+                promptDislikes = "";
+            }
+            else
+            {
+                promptDislikes = sectionDislikes + userDislikes;
+            }
+            userLikes = entryLikes.Text;
+            if (userLikes == "None")
+            {
+                promptLikes = "";
+            }
+            else
+            {
+                promptLikes = sectionLikes + userLikes;
+            }
+            //Expected output with sample variables: Write me a meal plan for the next week and give me only
+            //the headings of each meal, I am a Female, my diet is Vegan, I am allergic to Peanuts.
+            promptFull = promptOpen + promptSimple + ", " + promptAge + " year old " + userGender + promptActivity + promptMeals + promptDiet + promptAllergy + promptDislikes + promptLikes + ".";
+            return promptFull;
         }
     }
-
-    // means that the extended prompt answer will only show if circumstance is met
-    private void NoDietRadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
-    {
-        if (e.Value)
-        {
-            // If "No" is checked, hide the entry field
-            entryDiet.IsVisible = false;
-        }
-        else
-        {
-            // If "No" is unchecked, show the entry field
-            entryDiet.IsVisible = true;
-        }
-    }
-    private void NoAllergiesRadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
-    {
-        if (e.Value)
-        {
-            // If "No" is checked, hide the entry field
-            entryAllergy.IsVisible = false;
-        }
-        else
-        {
-            // If "No" is unchecked, show the entry field
-            entryAllergy.IsVisible = true;
-        }
-    }
-    private void NoDislikesRadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
-    {
-        if (e.Value)
-        {
-            // If "No" is checked, hide the entry field
-            entryDislikes.IsVisible = false;
-        }
-        else
-        {
-            // If "No" is unchecked, show the entry field
-            entryDislikes.IsVisible = true;
-        }
-    }
-
-    private void NoPreferencesRadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
-    {
-        if (e.Value)
-        {
-            // If "No" is checked, hide the entry field
-            entryLikes.IsVisible = false;
-        }
-        else
-        {
-            // If "No" is unchecked, show the entry field
-            entryLikes.IsVisible = true;
-        }
-    }
-    //MAKE CHECKBOX INTO STRING DEPENDING ON SELECTION
-    private string GetSelectedMeals()
-    {
-        List<string> selectedMeals = new List<string>();
-
-        if (BreakfastCheckbox.IsChecked)
-        {
-            selectedMeals.Add("Breakfast");
-        }
-
-        if (LunchCheckbox.IsChecked)
-        {
-            selectedMeals.Add("Lunch");
-        }
-
-        if (DinnerCheckbox.IsChecked)
-        {
-            selectedMeals.Add("Dinner");
-        }
-
-        if (SnacksCheckbox.IsChecked)
-        {
-            selectedMeals.Add("Snacks");
-        }
-
-        // Combine the selected meals into a string with "AND" before the last option
-        if (selectedMeals.Count > 1)
-        {
-            string lastMeal = selectedMeals.Last();
-            selectedMeals[selectedMeals.Count - 1] = "and " + lastMeal;
-        }
-
-        string selectedMealsString = string.Join(", ", selectedMeals);
-
-        return selectedMealsString;
-    }
-
-
-    //GET USER INPUT TOTAL METHOD
-    private async void OnPromptClicked(object sender, EventArgs e)
-    {
-
-        // Gather user input
-        string age = entryAge.Text;
-        
-        //PREFFERRED GENDER
-        string gender = "";
-        if (MaleRadioButton.IsChecked)
-        {
-            gender = "Male";
-        }
-        else if (FemaleRadioButton.IsChecked)
-        {
-            gender = "Female";
-        }
-        else if (NonBinaryRadioButton.IsChecked)
-        {
-            gender = "Non-Binary";
-        }
-        else if (OtherRadioButton.IsChecked)
-        {
-            gender = "NA"; 
-        }
-
-
-        //ACTIVITY LEVEL
-        string activity = "";
-        if (NoActivityRadioButton.IsChecked)
-        {
-            activity = "Low (0-1 days a week)";
-        }
-        else if (LittleActivityRadioButton.IsChecked)
-        {
-            activity = "Fair (1-2 days a week)";
-        }
-        else if (QuiteActiveRadioButton.IsChecked)
-        {
-            activity = "Active (2-4 days a week)";
-        }
-        else if (VeryActiveRadioButton.IsChecked)
-        {
-            activity = "Very Active (5+ days a week)";
-        }
-
-        //NUMBER OF MEALS DESIRED
-
-        string meals = GetSelectedMeals();
-
-
-
-        // SPECIFIC DIETS//
-        string diet = "";
-        
-        if (NoRestrictionsRadioButton.IsChecked)
-        {
-            diet = "Unrestricted";
-        }      
-        else if (VegetarianRadioButton.IsChecked)
-        {
-            diet = "Vegetarian";
-        }
-        else if (PescetarianRadioButton.IsChecked)
-        {
-            diet = "Pescetarian";
-        }
-        else if (GlutenFreeRadioButton.IsChecked)
-        {
-            diet = "Gluten-Free";
-        }
-        else if (HalalRadioButton.IsChecked)
-        {
-            diet = "Halal";
-        }
-        else if (OtherDietRadioButton.IsChecked)
-        {
-            diet = entryDiet.Text;
-        }
-
-
-        //ALLERGIES
-        string allergy;
-        if (YesAllergy.IsChecked)
-        {
-            allergy = entryAllergy.Text;
-        }
-        else
-        {
-            allergy = "nothing";
-        }
-
-
-
-        //DISLIKES//
-        string dislikes = entryDislikes.Text;
-
-
-        //LIKES//
-        string likes = entryLikes.Text;
-
-        
-        
-        
-        
-        // Display "Generating" on the button
-        PromptBtn.Text = "Generating...";
-
-        // Delay for 5 seconds
-        await Task.Delay(2000);
-
-        string generatedPrompt = PromptGenerator.WritePrompt(age, gender, activity, meals, diet, allergy, dislikes, likes);
-
-        // Navigate to the generation page
-        await Navigation.PushAsync(new GenerationPage());
-    }
-}
-        
